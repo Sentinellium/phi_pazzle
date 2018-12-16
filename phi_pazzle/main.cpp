1953456ac8d0e9edc14e7a6af2f2eca4524b8137
Sentinellium <34355821+Sentinellium@users.noreply.github.com>
2018-12-16 03:00:39 +0300

    Add files via upload

---
+++ phi_pazzle/main.cpp
@@ -0,0 +1,340 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<vector<vector<int>>> matrix(13);
+    for(int i=0;i<13;i++){
+        matrix[i].resize(9);
+    }
+    for(int i=0;i<13;i++){
+        for(int j=0;j<9;j++){
+        matrix[i][j].resize(4);
+            for(int k=0;k<4;k++){
+                matrix[i][j][k]=0;
+
+            }
+        }
+    }
+    matrix[0][4][0]=4;
+    matrix[0][7][0]=4;
+    matrix[1][8][0]=5;
+    matrix[2][5][0]=5;
+    matrix[2][6][0]=7;
+    matrix[2][8][0]=8;
+    matrix[3][8][0]=8;
+    matrix[5][5][0]=8;
+    matrix[6][7][0]=6;
+    matrix[6][8][0]=6;
+    matrix[7][4][0]=5;
+    matrix[8][3][0]=7;
+    matrix[8][6][0]=7;
+    matrix[9][2][0]=4;
+    matrix[9][6][0]=4;
+    matrix[10][0][0]=2;
+    matrix[10][1][0]=2;
+    matrix[10][6][0]=4;
+    matrix[11][1][0]=1;
+    matrix[11][4][0]=5;
+    matrix[11][6][0]=8;
+    matrix[12][2][0]=4;
+    matrix[12][5][0]=7;
+
+    matrix[0][4][1]=4; //matrix[*][*][n] where n=0 is for start points n=1 drawn points n=2 is a mark to completed ways
+    matrix[0][7][1]=4;
+    matrix[1][8][1]=5;
+    matrix[2][5][1]=5;
+    matrix[2][6][1]=7;
+    matrix[2][8][1]=8;
+    matrix[3][8][1]=8;
+    matrix[5][5][1]=8;
+    matrix[6][7][1]=6;
+    matrix[6][8][1]=6;
+    matrix[7][4][1]=5;
+    matrix[8][3][1]=7;
+    matrix[8][6][1]=7;
+    matrix[9][2][1]=4;
+    matrix[9][6][1]=4;
+    matrix[10][0][1]=2;
+    matrix[10][1][1]=2;
+    matrix[10][6][1]=4;
+    matrix[11][1][1]=1;
+    matrix[11][4][1]=5;
+    matrix[11][6][1]=8;
+    matrix[12][2][1]=4;
+    matrix[12][5][1]=7;
+
+    for(int i=12;i>=0;i--){
+        for(int j=0;j<9;j++){
+            cout <<matrix[i][j][0];
+        }
+        cout <<endl;
+    }
+
+    cout <<endl;
+    for(int i=0;i<13;i++){
+        for(int j=0;j<9;j++){
+            if(matrix[i][j][0]>0){
+                if((i+matrix[i][j][0]-1<13) && (matrix[i][j][0]==matrix[i-1+matrix[i][j][0]][j][0])){
+                        for(int k=i;k<i-1+matrix[i][j][0];k++){
+                            matrix[k][j][1]=matrix[i][j][0];
+                        }
+                        matrix[i][j][2] = 1;
+                        matrix[i-1+matrix[i][j][0]][j][2]=1;
+                        }
+                if((j+matrix[i][j][0]-1<9) && (matrix[i][j][0]==matrix[i][j-1+matrix[i][j][0]][0])){
+                        for(int k=j;k<j-1+matrix[i][j][0];k++){
+                            matrix[i][k][1]=matrix[i][j][0];
+                        }
+                        matrix[i][j][2] = 1;
+                        matrix[i][j-1+matrix[i][j][0]][2] = 1;
+                        }
+
+            }
+        }
+    }
+    cout <<endl <<endl;
+    for(int i=12;i>=0;i--){
+        for(int j=0;j<9;j++){
+            cout <<matrix[i][j][1];
+        }
+        cout <<endl;
+    }
+    int maxh,maxl,maxr=0;
+
+
+
+    for(int i=0;i<=12;i++){
+        for(int j=0;j<=8;j++){
+
+        if((matrix[i][j][0] != 0) && (matrix[i][j][2] == 0)){
+            //cout <<"hi";
+
+            if(j + matrix[i][j][0] - 1 > 8){
+                maxr = 8;
+            }
+            else{
+                maxr = j + matrix[i][j][0] - 1;
+            }
+
+            if(j - matrix[i][j][0] + 1 < 0){
+                maxl = 0;
+            }
+            else{maxr = j - matrix[i][j][0] + 1;}
+
+            if(i + matrix[i][j][0] - 1 > 12){
+            maxh = 12;
+            }
+            else{maxh = i + matrix[i][j][0] - 1;}
+
+            for(int par2=0; par2<=12;par2++){
+                for(int par1=0; par1<=8;par1++){
+                    if((matrix[par2][par1][2] == 0) && (matrix[par2][par1][0] == matrix[i][j][0])){
+                        vector<vector<int>> path(matrix[i][j][0]);
+                        for(int create=0;create<matrix[i][j][0];create++){
+                            path[create].resize(3);
+                        }
+                        cout <<endl<<endl;
+                        path[0][0] = i;
+                        path[0][1] = j;
+                        path[0][2] = 1;
+
+                        int q=0;
+                        int count=0;
+
+
+
+
+
+
+
+
+                        cout <<endl<<endl<<endl<<endl<<endl<<endl<<endl;
+                        for(int shot=0;shot<10000;shot++){
+
+                            if((count==1) && (q==0) && (path[q][2]>=5)){
+                                    break;
+                        }
+
+
+                            if((q+1==matrix[i][j][0]) && (path[q][0]==i) && (path[q][1]==j)){
+                                    q--;
+                                    cout<<"back first"<<endl;
+                                    path[q][2]++;
+                                    path[q+1][2]=0;
+                                    continue;
+                            }
+                            if((path[q][0]==11) && (path[q][1]==7))
+                            {
+                                //cout<<endl<<endl<<endl<<"HELL YEAH"<<endl<<endl;
+                            }
+
+                            cout <<q<<" "<<path[q][0]<<' '<<path[q][1] <<" "<<path[q][2]<<endl;
+                            if((q+1==matrix[i][j][0]) && (matrix[path[q][0]][path[q][1]][0]==matrix[i][j][0])){ //solution found counter
+                                count++;
+                                cout <<"found"<<endl<<endl;
+                                q--;
+                                for(int shit=0;shit<=q+1;shit++){
+                                    cout <<shit<<" "<<path[shit][0]<<' '<<path[shit][1] <<" "<<path[shit][2]<<endl;
+                                }
+
+                                path[q][2]++;
+                                path[q+1][2]=0;
+                                matrix[i][j][3]=1;
+                                if(count==2){
+                                    matrix[i][j][3]=2;
+                                    break;
+                                }
+                            }
+                            if(path[q][2]>4){
+                                q--;
+                                cout<<"back first"<<endl;
+                                path[q][2]++;
+                                path[q+1][2]=0;
+                                continue;
+                            }
+
+                            if((q+1==matrix[i][j][0]) && (matrix[path[q][0]][path[q][1]][0]!=matrix[i][j][0])){ // if the end is wrong
+                                q--;
+                                cout<<"back first"<<endl;
+                                path[q][2]++;
+                                path[q+1][2]=0;
+                                continue;
+                            }
+
+                            if(path[q][2]==5){
+                                break;
+                            }
+
+
+
+
+
+
+
+
+
+
+
+                            int bad1=0;
+                            for(int temp=q;temp>0;temp--){
+                                if((path[temp][0] == path[q][0]) && (path[temp][1] == path[q][1]+1)){
+                                    bad1 = 1;
+                                }
+                            }
+                            int bad2=0;
+                            for(int temp=q-1;temp>=0;temp--){
+                                if((path[temp][1] == path[q][1]) && (path[temp][0] == path[q][0]+1)){
+                                    bad2 = 1;
+                                }
+                            }
+                            int bad3=0;
+                            for(int temp=0;temp<q;temp++){
+                                if(((path[temp][0] == path[q][0]) && (path[temp][1]== path[q][1]-1))){
+                                    bad3 = 1;
+                                    //cout<<"houston we have a problem";
+                                }
+                            }
+                            int bad4=0;
+                            for(int temp=0;temp<q;temp++){
+                                if((path[temp][0] == path[q][0]-1) && (path[temp][1] == path[q][1])){
+                                    bad4 = 1;
+                                    //cout<<"this is not zero =="<<bad4<<endl;
+                                }
+
+                            }
+                            //cout<<bad4<<endl;
+
+
+
+
+
+
+                            if((path[q][1]<8) && (path[q][2]<=1) && (bad1!=1)){ //checking for x+1 move
+                                if(((matrix[path[q][0]][path[q][1]+1][1] == 0)) || ((q+2==matrix[i][j][0]) && (matrix[path[q][0]][path[q][1]+1][1] == matrix[i][j][0]))){
+                                    cout <<"right"<<bad1<<endl;
+                                    q++;
+                                    path[q][0]=path[q-1][0];
+                                    path[q][1]=path[q-1][1]+1;
+                                    path[q-1][2]=1;
+                                    path[q][2]=0;
+                                    continue;
+                                }
+                            }
+
+
+                            if((path[q][0]<12)&& (path[q][2]<=2) && (bad2!=1)){ //checking for y+1 move
+                                if(((matrix[path[q][0]+1][path[q][1]][1] == 0)) || ((q+2==matrix[i][j][0]) && (matrix[path[q][0]+1][path[q][1]][1] == matrix[i][j][0]))){
+                                    q++;
+                                    cout <<"up"<<endl;
+                                    path[q][1]=path[q-1][1];
+                                    path[q][0]=path[q-1][0]+1;
+                                    path[q-1][2]=2;
+                                    path[q][2]=0;
+                                    continue;
+                                }
+                            }
+
+
+                            if((path[q][1]>0)&& (path[q][2]<=3) && (bad3!=1)){ //checking for x-1 move
+                                //cout<<"leftie"<<"  ";
+                                if(((matrix[path[q][0]][path[q][1]-1][1] == 0)) || ((q+2==matrix[i][j][0]) && (matrix[path[q][0]][path[q][1]-1][1] == matrix[i][j][0]))){
+                                    q++;
+                                    cout<<"left"<<bad3<<endl;
+                                    path[q][0]=path[q-1][0];
+                                    path[q][1]=path[q-1][1]-1;
+                                    path[q-1][2]=3;
+                                    path[q][2]=0;
+                                    continue;
+                                }
+                            }
+
+
+                            if((path[q][0]>0) && (path[q][2]<=4) && (bad4!=1)){ //checking for y-1 move
+                                cout<<"hi";
+                                if(((matrix[path[q][0]-1][path[q][1]][1] == 0))  || ((q+2==matrix[i][j][0]) && (matrix[path[q][0]-1][path[q][1]][1] == matrix[i][j][0]))){
+                                    q++;
+                                    cout<<"down"<<bad4<<endl;
+                                    path[q][1]=path[q-1][1];
+                                    path[q][0]=path[q-1][0]-1;
+                                    path[q-1][2]=4;
+                                    path[q][2]=0;
+                                    continue;
+                                }//cout<<"hi";
+
+                            }
+                            cout<<"back second"<<endl;
+
+                            //cout <<q<<" "<<path[q][0]<<' '<<path[q][1] <<" "<<path[q][2]<<endl;
+                            q--;
+                            if(q<0){
+                                break;
+                            }
+                            path[q][2]++;
+                            path[q+1][2]=0;
+
+
+                        }
+
+
+                    }
+                }
+        }
+        }
+
+            }
+
+
+}
+    for(int i=12;i>=0;i--){
+        for(int j=0;j<9;j++){
+            cout <<matrix[i][j][3];
+        }
+        cout <<endl;
+    }
+
+
+}
+
